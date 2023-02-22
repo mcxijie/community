034ef1dab4a21beeda45b21355450d46306b4a9d
xijie <907725402@qq.com>
2023-02-22 23:22:07 +0800

    热帖排行

--- src/main/java/com/nowcoder/community/config/QuartzConfig.java
+++ src/main/java/com/nowcoder/community/config/QuartzConfig.java
@@ -1,6 +1,7 @@
 package com.nowcoder.community.config;
 
 import com.nowcoder.community.quartz.AlphaJob;
+import com.nowcoder.community.quartz.PostScoreRefreshJob;
 import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
 import org.springframework.context.annotation.Bean;
@@ -8,19 +9,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
 
-//配置 -> 数据库 -> 调用
+// 配置 -> 数据库 -> 调用
 @Configuration
 public class QuartzConfig {
 
-    //FactoryBean可简化Bean的实例化过程：
-    //1.通过FactoryBean封装Bean的实例化过程。
-    //2.将FactoryBean装配到Spring容器里。
-    //3.将FactoryBean注入给其他的Bean。
-    //4.该Bean得到的是FactoryBean所管理的对象实例。
+    // FactoryBean可简化Bean的实例化过程:
+    // 1.通过FactoryBean封装Bean的实例化过程.
+    // 2.将FactoryBean装配到Spring容器里.
+    // 3.将FactoryBean注入给其他的Bean.
+    // 4.该Bean得到的是FactoryBean所管理的对象实例.
 
-    //配置JobDetal
-    @Bean
-    public JobDetailFactoryBean alphaJobDetal() {
+    // 配置JobDetail
+    // @Bean
+    public JobDetailFactoryBean alphaJobDetail() {
         JobDetailFactoryBean factoryBean = new JobDetailFactoryBean();
         factoryBean.setJobClass(AlphaJob.class);
         factoryBean.setName("alphaJob");
@@ -30,8 +31,8 @@ public class QuartzConfig {
         return factoryBean;
     }
 
-    //配置Trigger(SimpleTriggerFactoryBean，CronTriggerFactoryBean)
-    @Bean
+    // 配置Trigger(SimpleTriggerFactoryBean, CronTriggerFactoryBean)
+    // @Bean
     public SimpleTriggerFactoryBean alphaTrigger(JobDetail alphaJobDetail) {
         SimpleTriggerFactoryBean factoryBean = new SimpleTriggerFactoryBean();
         factoryBean.setJobDetail(alphaJobDetail);
@@ -41,4 +42,28 @@ public class QuartzConfig {
         factoryBean.setJobDataMap(new JobDataMap());
         return factoryBean;
     }
+
+    // 刷新帖子分数任务
+    @Bean
+    public JobDetailFactoryBean postScoreRefreshJobDetail() {
+        JobDetailFactoryBean factoryBean = new JobDetailFactoryBean();
+        factoryBean.setJobClass(PostScoreRefreshJob.class);
+        factoryBean.setName("postScoreRefreshJob");
+        factoryBean.setGroup("communityJobGroup");
+        factoryBean.setDurability(true);
+        factoryBean.setRequestsRecovery(true);
+        return factoryBean;
+    }
+
+    @Bean
+    public SimpleTriggerFactoryBean postScoreRefreshTrigger(JobDetail postScoreRefreshJobDetail) {
+        SimpleTriggerFactoryBean factoryBean = new SimpleTriggerFactoryBean();
+        factoryBean.setJobDetail(postScoreRefreshJobDetail);
+        factoryBean.setName("postScoreRefreshTrigger");
+        factoryBean.setGroup("communityTriggerGroup");
+        factoryBean.setRepeatInterval(1000 * 60 * 2);
+        factoryBean.setJobDataMap(new JobDataMap());
+        return factoryBean;
+    }
+
 }
